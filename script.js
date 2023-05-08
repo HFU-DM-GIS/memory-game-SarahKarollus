d83acf9d466913268e3ce296401e2ecb9ff8fa6a
kristelti <115099573+kristelti@users.noreply.github.com>
2023-05-08 13:28:28 +0200

    Korrektur Rechtschreibfehler

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function generateRandomOrder (length) {
   return order.sort(() => Math.random() - 0.5);   // hier wird random sortiert.
 }
 
-//weist eine random reihenforge jeder karte zu
+//weist eine random reihenfolge jeder karte zu
 const randomOrder = generateRandomOrder(cards.length); // in randomOrder steht das, was bei der funktion generateRandomOrder rauskommt.
 cards.forEach((card, index) => {      // für jede karte gibt es einen index.
   card.style.order = randomOrder[index]; // durch den index der karten werden diese der stellen in der random reihe zugeordnet.
@@ -59,11 +59,11 @@ function flipCard() {
 // Überprüfung auf Kartenübereinstimmung
 function checkForMatch() {
   if (firstCard.dataset.card === secondCard.dataset.card) { // prüft ob die "dataset.card" Eigenschaften der karten übereinstimmen
-    disableCards();   // wenn sie übereinstimmen, wird die Funktion "disableCards" ausgeführt. die die Karten werden aus dem Spiel "entfernt"
+    disableCards();   // wenn sie übereinstimmen, wird die Funktion "disableCards" ausgeführt. die Karten werden aus dem Spiel "entfernt" bzw gefreezed
     return;
   }
 
-  unflipCards();  // wenn sie nicht übereinstimmen wird die Funktio "unflipCartds" ausgeführt und die Karten werden wieder umgedreht
+  unflipCards();  // wenn sie nicht übereinstimmen wird die Funktion "unflipCards" ausgeführt und die Karten werden wieder umgedreht
 }
 
 // Deaktiviert die ersten beiden Karten und setzt das Board zurück
@@ -73,24 +73,24 @@ function disableCards() {
   firstCard.removeEventListener("click", flipCard);   
   secondCard.removeEventListener("click", flipCard);
   
-  // die klassen "correct" werde hizugefügt um ein Paar zu symbolisieren
+  // die Klassen "correct" werden hizugefügt um ein Paar zu symbolisieren
   firstCard.classList.add("correct");
   secondCard.classList.add("correct");
  
-  //  firstCard, secondCard, flippedCard und lockBoard werden zurückgesetzt un es kann weiter gespielt werdenn.
+  //  firstCard, secondCard, flippedCard und lockBoard werden zurückgesetzt und es kann weiter gespielt werdenn.
   resetBoard();
 }
 
-// Dreht beiden Karten zurück wen diese nicht übereinstimmen 
+// Dreht beiden Karten zurück wenn diese nicht übereinstimmen 
 function unflipCards() {
   lockBoard = true;
   firstCard.classList.add("incorrect"); // fügt die klasse "incorrect" hinzu
   secondCard.classList.add("incorrect");  // fügt die klasse "incorrect" hinzu
 
-  // Das Board wird gesperrt -> es wird verhindert, dass keine weiteren karten ausgewählt werden können
-  setTimeout(() => {  // es wird ein Timer von 1000 ms gestartet bevor die Funktio fortgesetzt wird.
+  // Das Board wird gesperrt -> es wird verhindert, dass keine weiteren Karten ausgewählt werden können
+  setTimeout(() => {  // es wird ein Timer von 1000 ms gestartet bevor die Funktion fortgesetzt wird.
     
-    firstCard.classList.remove("selected", "incorrect");    // nach ablauf des Timers: klassen "selected"
+    firstCard.classList.remove("selected", "incorrect");    // nach Ablauf des Timers: klassen "selected"
     secondCard.classList.remove("selected", "incorrect");   // und "incorrect" werden von den Karte entfernt.
     
     firstCard.firstChild.classList.remove("flipped");     // die Klasse "flipped" wird von den ersten beiden Kindelementen beider Karten entfernt
